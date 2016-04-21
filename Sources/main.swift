c5a039093a6ebdfac4d6552a5109708055cd12ec
Boris Bügling <boris@icculus.org>
2016-04-21 11:34:46 +0200

    Experiment with calling static methods
    The var arg variants of JNI functions are obviously not imported, but we
    can use the ones which take `jvalue` arrays.

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,5 +1,11 @@
 import CJavaVM
 
+extension jvalue: CustomStringConvertible {
+    public var description: String {
+        return "\(self.i)"
+    }
+}
+
 func create_vm(vmPtr: UnsafeMutablePointer<UnsafeMutablePointer<JavaVM>>) -> UnsafeMutablePointer<JNIEnv> {
   var args = JavaVMInitArgs()
   args.ignoreUnrecognized = 0
@@ -14,14 +20,14 @@ func create_vm(vmPtr: UnsafeMutablePointer<UnsafeMutablePointer<JavaVM>>) -> Uns
   withUnsafeMutablePointers(&options, &env) { optionsPtr, envPtr in
     args.options = optionsPtr
 
-    "-Djava.class.path=./".withCString {
+    "-Djava.class.path=./hello_world".withCString {
         let str = $0
         options.optionString = UnsafeMutablePointer<Int8>(str)
         let ptr = UnsafeMutablePointer<UnsafeMutablePointer<Void>>(envPtr)
         retval = JNI_CreateJavaVM(vmPtr, ptr, &args)
     }
   }
-   
+
   if retval < 0 || env == nil {
     print("Unable to launch JVM: \(retval)")
   } else {
@@ -33,3 +39,19 @@ func create_vm(vmPtr: UnsafeMutablePointer<UnsafeMutablePointer<JavaVM>>) -> Uns
 
 var vm: UnsafeMutablePointer<JavaVM> = nil
 let env = create_vm(&vm)
+let jni = env.pointee.pointee
+
+let hello_class = jni.FindClass(env, "helloWorld")
+
+let main_method = jni.GetStaticMethodID(env, hello_class, "main", "([Ljava/lang/String;)V")
+jni.CallStaticVoidMethodA(env, hello_class, main_method, [])
+
+let square_method = jni.GetStaticMethodID(env, hello_class, "square", "(I)I")
+let number = jvalue(i: 20)
+let square_result = jni.CallStaticIntMethodA(env, hello_class, square_method, [number])
+print("\(number) square is \(square_result)")
+
+let power_method = jni.GetStaticMethodID(env, hello_class, "power", "(II)I")
+let exponent = jvalue(i: 3)
+let power_result = jni.CallStaticIntMethodA(env, hello_class, power_method, [number, exponent])
+print("\(number) raised to the \(exponent) power is \(power_result)")
